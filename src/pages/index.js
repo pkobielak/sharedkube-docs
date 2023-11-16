891b448d5a4812c0187553113832de5427c230c0
Patryk Kobielak <patryk.kobielak@gmail.com>
2023-11-16 18:09:04 +0100

    fix homepage name
    Signed-off-by: Patryk Kobielak <patryk.kobielak@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,6 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`${siteConfig.title}`}
       description="Documentation website for sharedkube project.">
       <HomepageHeader />
       <main>
